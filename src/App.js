7220fadd897524857674c9c05e5eecab662d3bfc
stuartfreaks <stuartfreaks@me.com>
2022-11-14 09:52:44 +0100

    Build for Deployment
    Build for deployment

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
+import React, { useState } from 'react';
+import Counter from './Counter'
+import CounterHooks from './CounterHooks'
 
-import './App.css';
+export const ThemeContext = React.createContext()
 
 function App() {
-  return ( 
-    <div>
-      <button>+</button>
-      <span>0</span>
-      <button>-</button>
-    </div>
-
-  
+  console.log("Render App")
+  const [theme, setTheme] = useState('red')
+  return (
+    <ThemeContext.Provider value={{ backgroundColor: theme }}>
+      Counter
+      <Counter initialCount={0} />
+      Counter Hooks
+      <CounterHooks initialCount={0} />
+      <button onClick={() => setTheme(prevTheme => {
+        return prevTheme === 'red' ? 'blue' : 'red'
+      })}>Toggle Theme</button>
+    </ThemeContext.Provider>
   )
 }
 
